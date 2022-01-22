032254a71f5d972657023ded8efba0ab5db097ca
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-01-22 13:43:15 -0600

    Imagenes en platillos

--- src/controllers/platillos.js
+++ src/controllers/platillos.js
@@ -4,18 +4,21 @@ const DB = [
     platillo: "Mole",
     descripcion:
       "Un tradicional platillo mexicano lleno de historia, una combinación de varios chiles y especias",
+    img:"https://elgourmet.s3.amazonaws.com/recetas/cover/mole-_9IMJjkql3RCWT1eg6AHUD8PKyZLh7o.png",
   },
   {
     id: 2,
     platillo: "Pozole",
     descripcion:
       "El pozole es una sopa tradicional mexicana hecha a base de granos de maíz nixtamalizados, comúnmente de la variedad cacahuazintle, al que se agregan carnes, verduras y especias muy variadas según la región.",
+    img:"https://img-global.cpcdn.com/recipes/recipes_116031_v1419127785_receta_foto_00116031-hdzuxutbgf0qf577tqri/680x482cq70/pozole-rojo-a-mi-estilo-foto-principal.jpg",
   },
   {
     id: 3,
     platillo: "Enchiladas",
     descripcion:
       "Las enchiladas es un plato que en México se elabora con tortilla de maíz enrollada y bañada en alguna salsa picante utilizando chile en su preparación.​",
+    img:"https://www.vvsupremo.com/wp-content/uploads/2015/10/900X570_Enchiladas-Morelia-Style-With-Chorizo-And-Potatoes.jpg",
   },
 ];
 
